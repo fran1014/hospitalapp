b50c0286314ece9e566027baea373ec99be0b6b7
Franklin <71532004+fran1014@users.noreply.github.com>
2023-05-28 02:38:59 +0200

    layout and styling

---
+++ .js/data.js
@@ -0,0 +1,124 @@
+const pacientes = new Array(
+
+  {
+    nombre: 'Lucia',
+    apellido: 'Alvarez',
+    edad: '54',
+    diagnostico: 'gripe',
+    numeroSS: 'Kt7689',
+  },
+
+  {
+    nombre: 'Frank',
+    apellido: 'Rocha',
+    edad: '44',
+    diagnostico: 'ebola',
+    numeroSS: 'Kt4289',
+  },
+  {
+    nombre: 'Jorge',
+    apellido: 'Mendez',
+    edad: '34',
+    diagnostico: 'pabcreatis',
+    numeroSS: 'DL7889',
+  },
+
+  {
+    nombre: 'Juan',
+    apellido: 'Pelayo',
+    edad: '44',
+    diagnostico: 'diabetes',
+    numeroSS: 'CT7649',
+  },
+
+  {
+    nombre: 'Victor',
+    apellido: 'Fernnadez',
+    edad: '23',
+    diagnostico: 'anorexia',
+    numeroSS: 're7459',
+  },
+
+  {
+    nombre: 'Oscar',
+    apellido: 'Roman',
+    edad: '64',
+    diagnostico: 'parkinson',
+    numeroSS: 'Kt7689',
+  },
+
+  {
+    nombre: 'Benson',
+    apellido: 'Bro',
+    edad: '54',
+    diagnostico: 'parkinson',
+    numeroSS: 'Kt7689',
+  },
+
+  {
+    nombre: 'Julian',
+    apellido: 'Ramiz',
+    edad: '54',
+    diagnostico: 'demencia',
+    numeroSS: 'Kt7689',
+  },
+
+  {
+    nombre: 'Ramon',
+    apellido: 'Vastao',
+    edad: '67',
+    diagnostico: 'hipertension',
+    numeroSS: 'Kt7689',
+  },
+
+  {
+    nombre: 'Tony',
+    apellido: 'Mars',
+    edad: '56',
+    diagnostico: 'asma',
+    numeroSS: 'Dt7a5',
+  },
+
+  {
+    nombre: 'Alfonso',
+    apellido: 'Bruma',
+    edad: '54',
+    diagnostico: 'catarro',
+    numeroSS: 'Gy7689',
+  },
+
+  {
+    nombre: 'Ana',
+    apellido: 'Austria',
+    edad: '33',
+    diagnostico: 'hypert',
+    numeroSS: 'S745689',
+  },
+
+  {
+    nombre: 'Victor',
+    apellido: 'Frosh',
+    edad: '33',
+    diagnostico: 'anemia',
+    numeroSS: 'kl7289',
+  },
+
+  {
+    nombre: 'Manolo',
+    apellido: 'Diaz',
+    edad: '43',
+    diagnostico: 'bulimia',
+    numeroSS: 'Kt7689',
+  },
+
+  {
+    nombre: 'Vanesa',
+    apellido: 'Broch',
+    edad: '61',
+    diagnostico: 'covid',
+    numeroSS: 'Kt3689',
+  },
+
+
+
+)
