a7e7afc77f390a90640310f08074cb8288269543
Franklin <71532004+fran1014@users.noreply.github.com>
2023-05-28 11:35:06 +0200

    Paint all patients

--- js/main.js
+++ js/main.js
@@ -10,4 +10,17 @@ function pintarUnPaciente(pPaciente, pDom) {
   const ul = document.createElement('ul');
   const h2 = document.createElement('h2');
   const div = document.createElement('div');
+  div.classList.add('diagnostico');
+
+  ul.innerHTML = `<li>Edad: ${pPaciente.edad}</li>
+  <li>Seguridad Social: ${pPaciente.numeroSS}</li>`
+
+  h2.textContent = `${pPaciente.nombre} ${pPaciente.apellido}`;
+
+  div.textContent = `${pPaciente.diagnostico}`;
+  article.append(h2, ul, div);
+  pDom.appendChild(article);
+
 }
+
+pintarPacientes(pacientes, sectionPacientes)
